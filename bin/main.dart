587203f815731bd91ef1a611f85d056e8aaa1ccc
mcclayac <anthony.mcclay@gmail.com>
2020-05-22 00:42:42 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+//code 2.15
+main(List<String> arguments) {
+  // declarations
+  List fruitCollection = ['Mango', 'Apple', 'Jack fruit'];
+  var myIntegers = [1, 2, 3];
+
+  // logic
+  print(myIntegers[2]);
+  print(fruitCollection[0]);
+}
